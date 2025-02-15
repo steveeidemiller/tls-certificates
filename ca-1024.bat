54ce9d9bba1d9b7f9960af359d57c8b721688e7f
Steve Eidemiller <steve.eidemiller@gmail.com>
2025-02-14 20:05:49 -0600

    Initial commit

---
+++ ca-1024.bat
@@ -0,0 +1,2 @@
+REM 10-year CA with SHA256 and 1024-bit for smaller ESP32/IOT devices
+openssl req -x509 -sha256 -days 3560 -nodes -newkey rsa:1024 -config ca-1024.cnf -extensions v3_req -keyout ca-1024.key -out ca-1024.crt
